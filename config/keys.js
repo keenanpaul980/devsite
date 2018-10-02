a2e3efd3eb04b533684311e1a31b58bd86ba5bf9
Keenan <keenanpaul980@gmail.com>
2018-10-02 19:40:51 +0800

    added comments in files

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
+//object containing config variables
 module.exports = {
     mongoURI : 'mongodb://keenan:keenan123@ds119853.mlab.com:19853/projectconnector'
 }
--- server.js
+++ server.js
@@ -1,10 +1,13 @@
+//require modules
 const express = require('express');
 const mongoose = require('mongoose');
 
+//require API routes
 const users = require('./routes/api/users');
 const profile = require('./routes/api/profile');
 const posts = require('./routes/api/posts');
 
+//assign express module to app var
 const app = express();
 //DB config
 const db = require('./config/keys').mongoURI;
@@ -14,16 +17,15 @@ mongoose.connect(db).then(() => {
     console.log('Connected to MongoDB succesfully.');
 }).catch(err => console.log(err));
 
-app.get('/', function(req, res){
-    res.send('Hello!');
-})
-
+//assign port number
 const port = process.env.PORT || 5000;
 
+//use API routes 
 app.use('/api/users', users);
 app.use('/api/profile', profile);
 app.use('/api/posts', posts);
 
+//use function to receive requests
 app.listen(port, function(req, res){
     console.log(`Server running on port ${port}`);
 })
